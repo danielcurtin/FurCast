d2ae153ad600a2cf57c54637b439a80aea480546
Daniel Curtin <dcurtin.work@gmail.com>
2023-04-19 16:26:02 -0600

    Add functionality for a dynamic route path

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -6,14 +6,12 @@ import { NavLink } from "react-router-dom";
 const Search = ({ setSearchLocation }) => {
   const [search, setSearch] = useState('');
 
-  const cleanSearch = () => {
-    return search.toLowerCase().replace(/\s + /g, '');
-  };
+  const cleanSearch = () => search.toLowerCase().replace(/\s + /g, '');
 
   return (
     <form role="search" className="search">
       <input type="text" className="search-bar" id="searchBar" value={search} onChange={event => setSearch(event.target.value)}></input>
-      <NavLink to={`/city/placeholder`} type="submit" className="search-button fa-solid fa-magnifying-glass" onClick={() => setSearchLocation(cleanSearch())}></NavLink>
+      <NavLink to={`/city/${cleanSearch()}`} type="submit" className="search-button fa-solid fa-magnifying-glass" onClick={() => setSearchLocation(cleanSearch())}></NavLink>
       <label htmlFor="searchBar">Enter U.S. City or Zip Code</label>
     </form>
   );
