dc61422333f2516ddd8891cb83a85af41a5ddf99
Daniel Curtin <dcurtin.work@gmail.com>
2023-04-19 16:42:34 -0600

    Add mock data because of limited api calls per day

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,7 +1,16 @@
 import React from "react";
 import './Weather.css';
 
+import { sanDiegoWeather } from "../../mock-data";
+
 const Weather = ({ humidity, temperature, uvIndex, weatherCode }) => {
+  
+  //temp for limited api calls/day
+  humidity = sanDiegoWeather.humidity;
+  temperature = sanDiegoWeather.temperature;
+  uvIndex = sanDiegoWeather.uvIndex;
+  weatherCode = sanDiegoWeather.weatherCode;
+
   return (
     <section>
       <h1>The humidity is: {humidity}%</h1>
