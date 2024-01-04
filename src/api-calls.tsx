87fd6acdbad192067ec8bfd4fd69b175180ce65c
Daniel Curtin <dcurtin.work@gmail.com>
2024-01-04 09:16:51 -0700

    Convert api-calls to ts

--- src/api-calls.tsx
+++ src/api-calls.tsx
@@ -1,4 +1,4 @@
-const getWeather = location => {
+const getWeather = (location: string) => {
   return fetch(`https://api.tomorrow.io/v4/weather/realtime?units=imperial&location=${location}&apikey=${process.env.REACT_APP_API_KEY}`)
   .then(res => {
     if (res.ok) {
